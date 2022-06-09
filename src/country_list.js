3076e774678ce15c65659cee0245d1170839261f
HarukiKinoshita <harukikinoshita@outlook.com>
2022-06-10 01:32:32 +0900

    Add capitol location

--- src/country_list.js
+++ src/country_list.js
@@ -3,115 +3,150 @@ export default [
     country: "EU",
     nato: false,
     eu: true,
+    location: [4.3819266, 50.8355193]
   },
   { 
     country: "Turkey",
     nato: true,
-    eu: false
+    eu: false,
+    location: [32.85, 39.93]
   },
   {
     country: "Canada",
     nato: true,
-    eu: false
+    eu: false,
+    location: [-75.695, 45.4247]
   },
   {
     country: "United States",
     nato: true,
-    eu: false
+    eu: false,
+    location: [-75.0724, 39.747]
   },
   {
     country: "United Kingdom",
     nato: true,
-    eu: false
+    eu: false,
+    location: [-0.1275, 51.5072]
   },
   {
     country: "France",
     nato: true,
-    eu: true
+    eu: true,
+    location: [2.3522, 48.8566]
   },
   {
     country: "Germany",
     nato: true,
     eu: true,
+    location: [13.3833, 52.5167]
   },
   {
     country: "Italy",
     nato: true,
     eu: true,
+    location: [12.4828, 41.8931]
   },
   { 
     country: "Poland", 
     nato: true,
-    eu: true
+    eu: true,
+    location: [21.0111, 52.23]
   },
   { 
     country: "Slovakia", 
     nato: true,
-    eu: true
+    eu: true,
+    location: [17.1128, 48.1447]
   },
   { 
     country: "Estonia", 
     nato: true,
-    eu: true
+    eu: true,
+    location: [24.745, 59.4372]
   },
   {
     country: "Latvia",
     nato: true,
     eu: true,
+    location: [24.1069, 56.9475]
   },
   { 
     country: "Lithuania",
     nato: true,
-    eu: true
+    eu: true,
+    location: [25.2833, 54.6833]
   },
   {
     country: "Bulgaria",
     nato: true,
-    eu: true
+    eu: true,
+    location: [23.3217, 42.6979]
   },
   {
     country: "Austria",
     nato: true,
-    eu: true
+    eu: true,
+    location: [16.3725, 48.2083]
   },
   {
     country: "Luxembourg",
     nato: true,
-    eu: true
+    eu: true,
+    location: [6.1328, 49.6106]
   },
   {
     country: "Norway",
     nato: true,
-    eu: true
+    eu: true,
+    location: [10.7528, 59.9111]
   },
   {
     country: "Finland",
     nato: false,
-    eu: true
+    eu: true,
+    location: [24.9342, 60.1756]
   },
   {
     country: "Sweden",
     nato: false,
-    eu: true
+    eu: true,
+    location: [18.0686, 59.3294]
   },
   {
     country: "Denmark",
     nato: false,
-    eu: true
+    eu: true,
+    location: [12.5615, 55.6805]
+  },
+  {
+    country: "Israel",
+    nato: false,
+    eu: false,
+    location: [34.78, 32.08]
   },
   { 
     country: "Japan",
     nato: false,
-    eu: false
+    eu: false,
+    location: [139.7744, 35.6839]
   },
   { 
-    country: "Israel",
+    country: "China",
     nato: false,
-    eu: false
+    eu: false,
+    location: [116.4075, 39.904]
+  },
+  { 
+    country: "India",
+    nato: false,
+    eu: false,
+    location: [77.5913, 12.9791]
   },
   {
     country: "UN",
     nato: false,
-    eu: false
-  },
+    eu: false,
+    location: [-73.9694214, 40.756386]
+  }
 ]

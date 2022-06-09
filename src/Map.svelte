11e58585a113fe56e9cc81337a1be8fd38bb905e
HarukiKinoshita <harukikinoshita@outlook.com>
2022-06-10 01:32:52 +0900

    Add UN voting information

--- src/Map.svelte
+++ src/Map.svelte
@@ -3,9 +3,11 @@
   import { geoAlbers, geoNaturalEarth1, geoConicEqualArea, geoPath } from "d3-geo";
   import { onMount } from "svelte";
   import { feature } from "topojson";
+  import voting from './voting.js';
+
+  // export let countryList;
   let selected;
   let features = [];
-  let paises = [];
 
   const projection = geoNaturalEarth1();
   const path = geoPath().projection(projection);
@@ -16,6 +18,25 @@
     ).then(d => d.json())
     features = feature(response, response.objects.countries).features;
   });
+
+  $: currentNum = 0;
+
+  // $: vueltos = `M${countryList.map(p => `${projection(p.location)}`).join('L')}`;
+  // $: vueltos = `M${data.meeting.map(p => `${projection(countryList.find(el => el.country.includes(p.country)).location)}`).join('L')}`;
+
+  $: color_approve = "#14B8A6"
+  $: color_against = "#F43F5E"
+  $: color_abstain = "#EAB308"
+  $: color_absent = "#64748B"
+
+  function selectColor(name, currentNum) {
+    if (voting[currentNum].approve.find(el => el == name)) return color_approve
+    else if (voting[currentNum].against.find(el => el == name)) return color_against
+    else if (voting[currentNum].abstain.find(el => el == name)) return color_abstain
+    else if (voting[currentNum].absent.find(el => el == name)) return color_absent
+    else return "rgba(255,255,255,0.2)"
+    // https://tailwindcss.com/docs/customizing-colors
+  }
 </script>
 
 <style>
@@ -25,19 +46,66 @@
   @media (prefers-color-scheme: dark) {
     .border {
       stroke: #041a28;
+      stroke-width: 0.5;
     }
+  }
+  .voting td {
+    padding: 4px 6px;
+    min-width: 10vw;
   }
 </style>
 
-<div class="selectedName">{selected?.properties.name ?? ''}</div>
+<div>
+  <button on:click={() => {currentNum = 0}}>Crimia</button>
+  <button on:click={() => {currentNum = 1}}>Ukraine</button>
+  <h2>{ voting[currentNum].name }</h2>
+  <table class="voting" align="center">
+    <tr>
+      <td style="background: {color_approve}">Approved</td>
+      <td style="background: {color_against}">Against</td>
+      <td style="background: {color_abstain}">Abstain</td>
+      <td style="background: {color_absent}">Absent</td>
+    </tr>
+    <tr style="font-size: 3rem; font-weight: bold;">
+      <td>{ voting[currentNum].approve.length }</td>
+      <td>{ voting[currentNum].against.length }</td>
+      <td>{ voting[currentNum].abstain.length }</td>
+      <td>{ voting[currentNum].absent.length }</td>
+    </tr>
+  </table>
+</div>
 
 <svg viewBox="0 0 1000 430" style="width: 100%; height: 100%;">
   <g>
     {#each features as feature, i}
-      <path d={path(feature)} class="border" on:click={() => {selected = feature, console.log(selected.id, feature.id)}} fill="rgba(255,255,255,0.1)" />
+      <path
+        d={path(feature)}
+        class="border"
+        fill={selectColor(feature.properties.name, currentNum)}
+        on:click={() => {selected = feature, console.log(selected.id, feature.id)}} 
+      />
     {/each}
   </g>
   {#if selected}
 		<path d={path(selected)} fill="hsl(0 0% 50% / 20%)" />
 	{/if}
+  <!-- <circle
+    class="pin"
+    cx={projection([30.5236, 50.45])[0]}
+    cy={projection([30.5236, 50.45])[1]}
+    r={2}
+    fill={'red'} /> -->
+  <!-- {#each countryList as pin}
+    <circle
+      class="pin"
+      cx={projection(pin.location)[0]}
+      cy={projection(pin.location)[1]}
+      r={2}
+      fill={'darkturquoise'}
+      fill-opacity={0.6}
+    />
+  {/each} -->
+  <!-- <path class="path-line" d={vueltos}></path> -->
 </svg>
+
+<div class="selectedName">{selected?.properties.name ?? ''}</div>
---
+++ src/voting.js
@@ -0,0 +1,36 @@
+export default [
+  {
+    code: "68/262",
+    name: "Annexation of Crimia in 2014",
+    description: "in response to the Russian annexation of Crimea",
+    approve: [
+      "Albania", "Andorra", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Barbados", "Belgium", "Benin", "Bhutan", "Bulgaria", "Cabo Verde", "Cameroon", "Canada", "Central African Rep.", "Chad", "Chile", "Colombia", "Costa Rica", "Croatia", "Cyprus", "Czechia", "Dem. Rep. Congo", "Denmark", "Dominican Rep.", "Estonia", "Finland", "France", "Georgia", "Germany", "Greece", "Guatemala", "Guinea", "Haiti", "Honduras", "Hungary", "Iceland", "Indonesia", "Ireland", "Italy", "Japan", "Jordan", "Kiribati", "Kuwait", "Latvia", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", "Malawi", "Mauritius", "Malaysia", "Maldives", "Malta", "Marshall Islands", "Mexico", "Micronesia", "Moldova", "Monaco", "Montenegro", "Netherlands", "New Zealand", "Niger", "Nigeria", "Macedonia", "Norway", "Palau", "Panama", "Papua New Guinea", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Samoa", "San Marino", "Saudi Arabia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "Spain", "South Korea", "Sweden", "Switzerland", "Thailand", "Togo", "Trinidad and Tobago", "Tunisia", "Turkey", "Ukraine", "United Kingdom", "United States of America"
+    ],
+    against: [
+      "Armenia", "Belarus", "Bolivia", "Cuba", "Nicaragua", "North Korea", "Russia", "Sudan", "Syria", "Venezuela", "Zimbabwe"
+    ],
+    abstain: [
+      "Afghanistan",	"Algeria",	"Angola",	"Antigua and Barbuda",	"Argentina",	"Bangladesh",	"Botswana",	"Brazil",	"Brunei",	"Burkina Faso",	"Burundi",	"Cambodia",	"China",	"Comoros",	"Djibouti",	"Dominica",	"Ecuador",	"Egypt",	"El Salvador",	"Eritrea",	"Ethiopia",	"Fiji",	"Gabon",	"Gambia",	"Guyana",	"India",	"Iraq",	"Jamaica",	"Kazakhstan",	"Kenya",	"Lesotho",	"Mali",	"Mauritania",	"Mongolia",	"Mozambique",	"Myanmar",	"Namibia",	"Nepal",	"Nauru",	"Pakistan",	"Paraguay",	"Rwanda",	"Saint Kitts and Nevis",	"Saint Lucia",	"Saint Vincent and the Grenadines",	"São Tomé and Príncipe",	"Senegal",	"South Africa",	"S. Sudan",	"Sri Lanka",	"Suriname",	"Swaziland",	"Tanzania",	"Uganda",	"Uruguay",	"Uzbekistan",	"Vietnam",	"Zambia"
+    ],
+    absent: [
+      "Belize",	"Bosnia and Herz.",	"Congo",	"Côte d'Ivoire",	"Eq. Guinea",	"Ghana",	"Grenada",	"Guinea-Bissau",	"Iran",	"Israel",	"Kyrgyzstan",	"Laos",	"Lebanon",	"Morocco",	"Oman",	"Serbia",	"Tajikistan",	"Timor-Leste",	"Tonga",	"Turkmenistan",	"Tuvalu",	"United Arab Emirates",	"Vanuatu",	"Yemen"
+    ]
+  },
+  {
+    code: "ES-11/1",
+    name: "Invasion of Ukraine in 2021",
+    approve: [
+      "Afghanistan", "Albania", "Andorra", "Antigua and Barbuda", "Argentina", "Australia", "Austria", "Bahamas", "Bahrain", "Barbados", "Belgium", "Belize", "Benin", "Bhutan", "Bosnia-Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Cambodia", "Canada", "Cape Verde", "Chad", "Chile", "Colombia", "Comoros", "Costa Rica", "Côte d'Ivoire", "Croatia", "Cyprus", "Czechia",	"Dem. Rep. Congo",	"Denmark",	"Djibouti",	"Dominica",	"Dominican Rep.",	"Ecuador",	"Egypt",	"Estonia",	"Fiji",	"Finland",	"France",	"Gabon",	"Gambia",	"Georgia",	"Germany",	"Ghana",	"Greece",	"Grenada",	"Guatemala",	"Guyana",	"Haiti",	"Honduras",	"Hungary",	"Iceland",	"Indonesia",	"Ireland",	"Israel",	"Italy",	"Jamaica",	"Japan",	"Jordan",	"Kenya",	"Kiribati",	"Kuwait",	"Latvia",	"Lebanon",	"Lesotho",	"Liberia",	"Libya",	"Liechtenstein",	"Lithuania",	"Luxembourg",	"Malawi",	"Malaysia",	"Maldives",	"Malta",	"Marshall Islands",	"Mauritania",	"Mauritius",	"Mexico",	"Micronesia",	"Moldova",	"Monaco",	"Montenegro",	"Myanmar",	"Nauru",	"Nepal",	"Netherlands",	"New Zealand",	"Niger",	"Nigeria",	"Macedonia",	"Norway",	"Oman",	"Palau",	"Panama",	"Papua New Guinea",	"Paraguay",	"Peru",	"Philippines",	"Poland",	"Portugal",	"Qatar",	"Republic of Korea",	"Romania",	"Rwanda",	"Saint Kitts and Nevis",	"Saint Lucia",	"Saint Vincent and Grenadines",	"Samoa",	"San Marino",	"São Tomé and Príncipe",	"Saudi Arabia",	"Serbia",	"Seychelles",	"Sierra Leone",	"Singapore",	"Slovakia",	"Slovenia",	"Solomon Islands",	"Somalia",	"Spain",	"Suriname",	"Sweden",	"Switzerland",	"Thailand",	"Timor-Leste",	"Tonga",	"Trinidad and Tobago",	"Tunisia",	"Turkey",	"Tuvalu",	"Ukraine",	"United Arab Emirates",	"United Kingdom",	"United States of America",	"Uruguay",	"Vanuatu",	"Yemen",	"Zambia"			
+    ],
+    against: [
+      "Belarus", "Eritrea", "North Korea", "Russia", "Syria"
+    ],
+    abstain: [
+      "Algeria",	"Angola",	"Armenia",	"Bangladesh",	"Bolivia",	"Burundi",	"Central African Rep.",	"China",	"Congo",	"Cuba",	"El Salvador",	"Eq. Guinea",	"India",	"Iran",	"Iraq",	"Kazakhstan",	"Kyrgyzstan",	"Laos",	"Madagascar",	"Mali",	"Mongolia",	"Mozambique",	"Namibia",	"Nicaragua",	"Pakistan",	"Senegal",	"South Africa",	"S. Sudan",	"Sri Lanka",	"Sudan",	"Tajikistan",	"Tanzania",	"Uganda",	"Vietnam",	"Zimbabwe"
+    ],
+    absent: [
+      "Azerbaijan",	"Burkina Faso",	"Cameroon",	"Ethiopia",	"eSwatini",	"Guinea",	"Guinea-Bissau",	"Morocco",	"Togo",	"Turkmenistan",	"Uzbekistan",	"Venezuela"
+    ]
+  }
+]
+
